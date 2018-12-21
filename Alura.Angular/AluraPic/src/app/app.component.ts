b4a698e170516c25db6941a49906475cc5243990
Rafael Correia <rafaeljpc@gmail.com>
2018-12-21 14:35:43 -0200

    Alura Angular Aula 3

---
+++ Alura.Angular/AluraPic/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Alura Pic';
+    photos: Object[] = [
+    ];
+
+    /**
+     *
+     */
+    constructor(photoService: PhotoService) {
+        photoService.listFromUser('flavio').subscribe(
+            photos => this.photos = photos
+        );
+    }
+}
