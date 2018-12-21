6add35c35ec2f038cd5ec15701adceb71b9653f4
Rafael Correia <rafaeljpc@gmail.com>
2018-12-21 13:10:34 -0200

    Alura Angular Aula 2

---
+++ AluraPic/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photo.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
