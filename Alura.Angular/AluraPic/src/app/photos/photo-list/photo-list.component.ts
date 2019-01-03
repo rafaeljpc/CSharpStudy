50713eb7a596ea968021eaf4ebe08fd2a6c2ad86
Rafael Correia <rafaeljpc@gmail.com>
2019-01-03 15:33:55 -0200

    Alura Angular Aula 5

--- Alura.Angular/AluraPic/src/app/photos/photo-list/photo-list.component.ts
+++ Alura.Angular/AluraPic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -8,27 +11,43 @@ import { PhotoService } from '../photo/photo.service';
     templateUrl: './photo-list.component.html',
     styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-    photos: Object[] = [
-    ];
+    photos: Photo[] = [];
     userName: string;
+    filter = '';
+    debounce: Subject<string> = new Subject<string>();
+    hasMore = true;
+    currentPage = 1;
 
     /**
      *
      */
     constructor(
-        private photoService: PhotoService,
-        private activatedRoute: ActivatedRoute
+        private activatedRoute: ActivatedRoute,
+        private service: PhotoService,
     ) { }
 
     ngOnInit(): void {
         this.userName = this.activatedRoute.snapshot.params.userName;
-        this.photoService
-            .listFromUser(this.userName)
-            .subscribe(
-                photos => this.photos = photos
-            );
+        this.photos = this.activatedRoute.snapshot.data['photos'];
+        this.debounce
+            .pipe(debounceTime(300))
+            .subscribe(filter => this.filter = filter);
     }
 
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
+    }
+
+    load() {
+        this.service
+            .listFromUserPaginated(this.userName, ++this.currentPage)
+            .subscribe(photos => {
+                this.photos = this.photos.concat(...photos);
+                if (!photos.length) {
+                    this.hasMore = false;
+                }
+            });
+    }
 }
