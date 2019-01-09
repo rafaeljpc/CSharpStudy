3ff59283e804ad1cda827888f0f4df1c6e953009
Rafael Correia <rafaeljpc@gmail.com>
2019-01-09 12:20:11 -0200

    Alura Angular M2 Aula 5 - request.interceptor

---
+++ Alura.Angular/AluraPic/src/app/core/auth/request.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+} from '@angular/common/http';
+import { TokenService } from '../token/token-service';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+    constructor(private tokenService: TokenService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        if (this.tokenService.hasToken()) {
+            const token = this.tokenService.getToken();
+            req = req.clone({
+                setHeaders: {
+                    'x-access-token': token
+                }
+            });
+        }
+
+        return next.handle(req);
+    }
+}
