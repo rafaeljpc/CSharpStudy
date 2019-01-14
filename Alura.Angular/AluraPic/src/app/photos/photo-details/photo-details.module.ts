2ad0bc6b29d811ccdbfed13b5b9e05ba61654a4d
Rafael Correia <rafaeljpc@gmail.com>
2019-01-14 15:42:07 -0200

    Alura Angular M3 Aula 4

--- Alura.Angular/AluraPic/src/app/photos/photo-details/photo-details.module.ts
+++ Alura.Angular/AluraPic/src/app/photos/photo-details/photo-details.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
+import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/componentes/vmessage/vmessage.module';
 
 @NgModule({
-    declarations: [PhotoDetailsComponent],
+    declarations: [
+        PhotoDetailsComponent,
+        PhotoCommentsComponent
+    ],
     imports: [
         CommonModule,
-        PhotoModule
+        PhotoModule,
+        RouterModule,
+        ReactiveFormsModule,
+        VMessageModule
     ],
-    exports: [PhotoDetailsComponent],
+    exports: [
+        PhotoDetailsComponent,
+        PhotoCommentsComponent
+    ],
     providers: [],
 })
 export class PhotoDetailsModule {}
--- Alura.Angular/AluraPic/src/app/photos/photo/photo.service.ts
+++ Alura.Angular/AluraPic/src/app/photos/photo/photo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from './photo';
+import { PhotoComment } from './photo-comment';
 
 const API = 'http://localhost:3000';
 
@@ -28,7 +29,18 @@ export class PhotoService {
         return this.http.post(API + '/photos/upload', formData);
     }
 
-    findById(id: string) {
-        return this.http.get<Photo>(API + '/photos/' + id);
+    findById(photoId: number) {
+        return this.http.get<Photo>(API + '/photos/' + photoId);
+    }
+
+    getComments(photoId: number) {
+        return this.http.get<PhotoComment[]>(API + '/photos/' + photoId + '/comments');
+    }
+
+    addComment(photoId: number, commentText: string) {
+        return this.http.post<PhotoComment[]>(
+            API + '/photos/' + photoId + '/comments',
+            { commentText }
+        );
     }
 }
