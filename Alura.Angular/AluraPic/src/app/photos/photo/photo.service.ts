1b362fe56bfa7074b95412a674d4ed0f446f8df5
Rafael Correia <rafaeljpc@gmail.com>
2019-01-14 16:14:48 -0200

    Alura Angular M3 Aula 5

--- Alura.Angular/AluraPic/src/app/photos/photo/photo.service.ts
+++ Alura.Angular/AluraPic/src/app/photos/photo/photo.service.ts
@@ -43,4 +43,8 @@ export class PhotoService {
             { commentText }
         );
     }
+
+    removePhoto(photoId: number) {
+        return this.http.delete(API + '/photos/' + photoId);
+    }
 }
